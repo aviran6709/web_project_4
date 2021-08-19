1048d53ecf11de67e5b698eba092db2698a0c324
aviran6709 <aviran670@gmail.com>
2021-08-20 00:40:02 +0300

    final ver

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# web_project_4
+# Project 4: Around The U.S.
+
+###This site is sprint 4 that made for practicum
+a profile and card image
+eround the world
+Photos and wonderful places and contains the following
+1 responsive web for many screen
+*1280
+*1024
+*500
+*320
+2 great lakes and parkes over us
+
+3 grid layers with wonderful photo
+
+The site is made up of flexible layers flex box, grid
+used media quarry for breakpoint
+from any device
+and have functionality
+to update name and job
+(https://aviran6709.github.io/web_project_3/index.html)
+And was created under the guidance of Practicum 100
+(https://www.yandex.com)
+
+made with HTML and CSS JS
+By Aviran
---
+++ script.js
@@ -0,0 +1,33 @@
+//declerision
+let editButton = document.querySelector(".profile__button_pen");
+let popUpOpen = document.querySelector(".popup");
+let closeBtn = document.querySelector(".popup__button_close");
+
+
+//funcsion work when click on edit btn
+editButton.addEventListener("click",  function(){
+popUpOpen.classList.add('popup__opened')
+
+});
+//funcsion work when click on x btn to close popup
+closeBtn.addEventListener("click",  function(){
+popUpOpen.classList.remove('popup__opened')
+    
+    });
+
+let submitBtn = document.querySelector(".popup__submit-btn");
+//function to save the info submit-btn
+submitBtn.addEventListener("click",  function(){
+    let profileName = document.querySelector(".profile__title"); 
+    let profileHobby = document.querySelector(".profile__hobby"); 
+    let  userName = document.querySelector(".popup__input_user_name")
+     //set the input text 
+    profileName.textContent = userName.value 
+   
+    let  Hobby = document.querySelector(".popup__input_hobby")
+    profileHobby.textContent = Hobby.value 
+
+    });
+
+
+    
