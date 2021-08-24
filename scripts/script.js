c7b3c64fbca843c2641f67f6c9639bba275b9ca3
aviran6709 <aviran670@gmail.com>
2021-08-24 16:06:02 +0300

    fix js btn

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
 
 //declerision
 
-let profileName = document.querySelector(".profile__title")
-let profileHobby = document.querySelector(".profile__hobby")
+let profileName = document.querySelector(".profile__title");
+let profileHobby = document.querySelector(".profile__hobby");
 let editButton = document.querySelector(".profile__edit-btn");
 let popUpOpen = document.querySelector(".popup");
 let closeBtn = document.querySelector(".popup__close-btn");
-
+let formElement = document.querySelector(".popup__content");
+let nameInput = document.querySelector(".popup__input_user_name");
+let jobInput =  document.querySelector(".popup__input_hobby");
 
 function popOpen(){
-    popUpOpen.classList.toggle('popup_opened')
-};
+    popUpOpen.classList.toggle('popup_opened');
+    if (popUpOpen.classList.contains('popup_opened')){
+        nameInput.value = profileName.textContent ;
+        jobInput.value = profileHobby.textContent ;
+        console.log(profileName.textContent )
+    };
+}
 
 //funcsion work when click on edit btn
 editButton.addEventListener("click",  popOpen);
@@ -19,19 +26,16 @@ closeBtn.addEventListener("click",  popOpen);
 
 
 
-let formElement = document.querySelector(".popup__content");
+
 
 
 function handleFormSubmit(evt) {
 evt.preventDefault();
-let nameInput = document.querySelector(".popup__input_user_name")
-let jobInput =  document.querySelector(".popup__input-hobby")
-
-profileName.textContent = nameInput.value 
-profileHobby.textContent = jobInput.value 
+profileName.textContent = nameInput.value ;
+profileHobby.textContent = jobInput.value ;
 
 popOpen()
-};
+}
 
 // Connect the handler to the form:
 // it will watch the submit event
